59459df52a946203537da8c671d2a05c7a5e7ead
ultom <ultom@DESKTOP-HHJ2Q5T.DLink>
2022-05-05 15:13:15 -0400

    Extra Credit due 5/5/2022

--- src/main/java/com/mycompany/extra_credit_assignment/extra_credit.java
+++ src/main/java/com/mycompany/extra_credit_assignment/extra_credit.java
@@ -1,5 +1,14 @@
 //Extra credit assingment 
 //Kevin Carpenter
+
+//Develop an algorithm (Java or C++) that can find the ratio of the sum of the largest two numbers divided by the sum of the smallest two numbers in an array. 
+//The solution should be linear or better.  
+//For example:
+//If the input is 
+//int[] a={3,5,12,450,1,1,5,6,23,6,8,9,321,2,3};
+//The ratio is (450+321) / (1+1) = 385.5. 
+
+
 package com.mycompany.extra_credit_assignment;
 
 
